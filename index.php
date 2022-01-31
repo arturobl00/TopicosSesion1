0d8a586835469f2ef8d5845aa9f2e884abb07133
arturobl00 <arturobl00@msn.com>
2022-01-31 09:32:38 -0600

    Primera Sesion

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Topicos Avanzados de Programación</title>
+</head>
+<body>
+    <div class="container mt-3">
+    <h2>Primera Unidad Interfaces</h2>
+  <div class="mt-4 p-5 bg-primary text-white rounded">
+    <h1>Bienvenidos al Curso Febrero - Junio 2022</h1>
+    <p>MTI. José Arturo Bustamante Lazcano <br/> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..</p>
+  </div>
+  <br/>
+<div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>
+</div>
+</body>
+</html>
