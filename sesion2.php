c71cf67552a2ccd479b429dc45a27d7208e92105
arturobl00 <arturobl00@msn.com>
2022-02-02 09:03:57 -0600

    sesion3

--- sesion2.php
+++ sesion2.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Manejando Textos</title>
 </head>
 <body>
     <!--Div container-->
-    <div class="container pt-5">
+    <div class="container pt-1">
         <!-- Primera practica usando encabezados-->
         <h1>Soy la etiqueta h1 y soy un encabezado</h1>
         <h2>Soy la etiqueta h2 y soy un encabezado</h2>
